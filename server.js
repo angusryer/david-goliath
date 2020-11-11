c7509fe051267e7b85e38216e49d85b495aeb09e
Angus Ryer <angus@enabled.company>
2020-11-11 06:09:12 -0500

    added code to serve index.html from server

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const app = require('express')();
-const listItems = require('./data/data.json');
+
 require('dotenv').config();
 
 const port = process.env.PORT || 5001;
+const path = require('path')
+const listItems = require('./data/data.json');
+
+app.use(express.static(path.join(__dirname, 'client/build')))
+app.get('*', (_req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'))
+})
 
 app.get('/', (_req, res) => {
   res.status(200).send(listItems);
